ee4e25049561a57493907a9d5f87688115dc8e4d
Jeff Ely <jeff.ely.08@gmail.com>
2015-03-22 15:30:26 -0400

    absolute julian day calcs complete

--- solar.py
+++ solar.py
@@ -1,6 +1,6 @@
 
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class solar:
     """
@@ -13,11 +13,11 @@ class solar:
         """initializes critical  spatial information"""
 
         # attributes
-        self.lat    = float(lat)        # lattitude - float
-        self.lon    = float(lon)        # longitude - float
-        self.rdt    = []                # exact datetime of solar object
-        self.tz     = 0.0               # time zone (from GTM) - float
-
+        self.lat    = float(lat)        # lattitude (E positive)- float
+        self.lon    = float(lon)        # longitude (N positive)- float
+        self.rdt    = []                # ref_datetime. Precise GMT time of solar obj
+        self.tz     = 0.0               # timezone, offset from GMT - float
+        self.adj    = 0.0               # absolute julian day
         pass
 
 
@@ -31,24 +31,42 @@ class solar:
         # if input is datetime_obj set it
         if isinstance(date_time_obj, datetime):
             self.rdt = date_time_obj
-            return
 
         if isinstance(date_time_obj, str) and isinstance(fmt, str):
             self.rdt = datetime.strptime(date_time_obj,fmt)
-            return
         else:
             raise Exception("bad datetime!")
-    
+
+        self.abs_julian_day()
+        return
+
+
+    def set_timezone(self, GMT_hour_offset):
+        """ amends the datetime object with time zone information"""
+
+        # sets time zone in a way to prevent accidental repeated offsetting
+        if self.tz != GMT_hour_offset:
+            time_del = timedelta(hours = (GMT_hour_offset - self.tz))
+            self.rdt = self.rdt + time_del
+            self.tz  = GMT_hour_offset
+
+        self.abs_julian_day()
+        return 
+
         
     def abs_julian_day(self):
-        """ calculates the ABSOLUTE julian day"""
+        """
+        calculates the ABSOLUTE decimal julian day of ref_datetime
 
-        pass
-    
+        This is invoked whenever set_datetime or set_timezone is invoked
+        """
 
-    def timezone(self, time_shift):
-        """ amends datetime object with timezone information"""
+        # uses the reference day of january 1st 2000
+        jan_1st_2000_jd   = 2451545
+        jan_1st_2000      = datetime(2000,1,1,12,0,0)
 
+        time_del = self.rdt - jan_1st_2000
+        self.ajd = float(jan_1st_2000_jd) + float(time_del.total_seconds())/86400
         pass
     
 
@@ -121,4 +139,21 @@ class solar:
 
         pass
 
+
+if __name__ == "__main__":
+
+    s = solar(37.0349, -76.3601)
+    s.set_datetime("20150322120000","%Y%m%d%H%M%S")
+    s.set_timezone(-5)
+    print("abs julian day:",s.ajd)
+
+
+
+
+
+
+
+
+
+    
         
