4299ab9ae6451f7cd963fb2c7e20f33f33f9e4fe
Jeff Ely <jeff.ely.08@gmail.com>
2015-04-05 12:17:06 -0400

    added chunking

--- chunk.py
+++ chunk.py
@@ -1,51 +1,157 @@
 
 
+__author__ = ["Jeffry Ely, Jeff.ely.08@gmail.com"]
+
+# standard imports
+import numpy
+
+
 class chunk_bundle():
     """
     Creates a chunk bundle object.
 
     it can be used to pass smaller pieces of information to complex functions
-    and reduce memory consumption in those functions. It can drastically
-    decrease performance, since it forces writing data to hard disk, so do not
-    use this unless you would otherwise run out of memory.
+    and reduce memory consumption in those functions.
+
+    Presently, chunks are not saved individually, but are always bundled to form the
+    undivided raster image. This allows chunks to be passed individually and sequentially
+    through large data processing tasks with each task taking much less memory. Then output
+    chunks may be stiched together in a chunk_bundle and saved as a raster image.
+
+    In the future, writing chunks to disk and dropping them from memory may be a good idea
+    to truly maximize the data volume that a limited memory space can handle.
+
+    FUTURE WORK NOTE:
+    For intended functionality, this module uses a dnppy wrapper of the arcpy.RasterToNumPyArray
+    function that passes a numpy array and a metadata object.
+    
+    This module SHOULD allow users without arcmap to import non-geospatial image data by using a library
+    such as python "cv2" for maximum utility.
     """
 
-    def __init__(self, name, numpy_array, metadata, num_chunks, temp_dir):
+    def __init__(self, rasterpath, num_chunks = 0, chunk_list = [], metadata = None, force_scv = False):
+        """
+        Creates a chunk bundle.
 
-        self.name = name            # name of object, used for saving chunks
-        self.data = numpy_array     # input raster numpy array
-        self.meta = metadata        # metadata object
-        self.subs = num_chunks      # number of chunks to split raster into
-        self.work = temp_dir        # temporary workspace to save chunks
+        Two probable use cases:
+            1) loading raster to split into smaller chunks with:
+                inchunk = chunk_bundle(rasterpath, num_chunks = #)
+                inchunk.load()
+                
+            2) building new chunk_bundle with processed data, passing on old chunks metadata:
+                outchunk = chunk_bundle(rasterpath, chunk_list = [chunk1, chunk2,...], metadata = metadata)
+                outchunk.save()
+        """
+
+        self.rasterpath = rasterpath        # full filepath to raster location
+        self.num_chunks = num_chunks        # number of chunks to subdivide or construct this into
+        self.chunk_list = chunk_list        # list of chunk objects (consitutent chunks)
+        self.metadata   = metadata          # raster metadata object for this chunk
+
+        self.force_scv  = force_scv         # forces simple CV module to be used instead of dnppy.
+                                            #   good for machines without arcmap installed.
 
         return
 
-    def split(self, num_chunks): pass
 
-    def pull(self, chunk_id):
-        """ reads a specific chunk and returns the info"""
-        pass
+    def __getitem__(self, index):
+        """ allows builtin __getitem__ to be used to get chunks by their integer ID numbers """
 
-    def push(self, chunk_id):
-        """ replaces a chunk with the input chunk data"""
-        pass
+        for chunk_obj in self.chunk_list:
+            if chunk_obj.index == index:
+                return chunk_obj
+            
+        else: raise Exception("No chunk with chunk_id = {0}".format(index))
 
-    def save(self): pass
+        
+    def read(self):
+        """ loads a raster image and splits it into roughly equal width vertical slices"""
+
+        if self.num_chunks <1:
+            raise Exception("Cannot split into any fewer than 1 chunk!")
+
+        # loads entire raster as numpy array with metadata object
+        if not self.force_scv:
+            
+            from dnppy import raster
+            data, self.metadata = raster.to_numpy(self.rasterpath)
+
+        # uses the simpleCV module to import raster without metadata  
+        else: pass
+            
+
+
+        # split the data and add new chunks to this raster
+        xs, ys = data.shape
+        width  = xs / float(self.num_chunks)
+
+        for c in range(self.num_chunks):
+            
+            chunk_data  = data[int(c * width):int((c+1) * width),:]
+            new_chunk   = chunk(c, chunk_data)
+            
+            self.chunk_list.append(new_chunk)
+
+        del data
+        return
+    
+
+    def write(self, rasterpath = None):
+        """ writes the chunk_bundle to its rasterpath (allows new filepath input) """
+
+        if not rasterpath:
+            rasterpath = self.rasterpath
+            
+        # write with metadata using dnppy and arcpy.
+        if self.metadata and not self.force_scv:
+            raster.from_numpy()
+
+        # write without metadata using simple CV  
+        else: pass
+
+
+
+
+        return
+
+
+    def ingest(self, new_chunk_obj):
+        """ places a chunk into the chunk bundle. It should replace an existing chunk with that ID"""
+
+        # delete any chunk already existing at the index location of the new chunk object
+        for chunk_obj in self.chunk_list:
+            if chunk_obj.index == index:
+                self.chunk_list.remove(new_chunk_obj.index)
+
+        self.chunk_list.append(new_chunk_obj)
+        return
+
 
 
 class chunk():
-    """ creates an individual chunk object. chunk_bundles are made of these"""
+    """
+    creates an individual chunk object (vertical slice of a raster image).
 
-    def __init__(self, chunk_id, numpy_array = None, filepath = None)
+    Individual chunks do NOT store metadata! Any metadata should be managed
+    at the chunk_bundle level. Object chunk_bundles are made of chunk lists,
+    ordered by index number.
+    """
 
+    def __init__(self, index, data = None):
+        """ initializes a chunk object. """
 
-        self.name = chunk_id
+        self.index  = index
+        self.data   = data
         
-        if numpy_array:
-            self.data = numpy_array
-
-        if filepath:
-            self.path = filepath
+        return
 
             
+    
+if __name__ == "__main__":
+
+
+    path = "test_path"
+    num  = 10
+    
+    c = chunk_bundle(path, num)
     
